562683a551cde8a997f9a7225697a7bc7d6e0c8f
w201rdada-portfolio-aassdd654-cut27sot <w201rdada-portfolio-aassdd654-cut27sot@local>
2017-12-02 16:24:11 +0000

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
 # put your info here and uncomment so git (and GitHub) knows who your commits belong to!
-git config --global user.email "lisa.chuli@gmail.com"
-git config --global user.name "aassdd654"
-
-#delete old files
-#rm /home/rstudio/docs/
-
-#copy _book files to folder docs
-cp -a /home/rstudio/_book/. /home/rstudio/docs/
-
-#cd _book
-
-#commit all the changes
-git rm -rf *
-git add -f *
-git commit -m"Update the book"
-git push -q origin
+# git config user.email "calnet@berkeley.edu"
+# git config user.name "Oski Bear"
 
 # make sure you're on the master branch
 git checkout master
+
+cd /home/rstudio/ \
+&& rm -rf docs/ \
+&& mv -u -T _book/ docs/
+
+# assuming you've already built it, replace old docs with new _books build
+echo "User-agent: *
+Disallow: /" > docs/robots.txt
+
+# site will be public so dissuade robots and search engines from crawling your page
+git add . \
+&& git commit -m "deploy" || true \
+&& echo '
+Ready to push with authorization.
+In Rstudio use menu "Tools > Shell..." and enter:
+
+git push origin master
+	
+Or from an open Rstudio project use menu
+"Git > Push Branch" or use tab "Git > Push".'
