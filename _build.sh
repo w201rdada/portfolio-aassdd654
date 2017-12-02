45bccf5c4643a27be0c6335b3c1ae47f552b7c6b
Brooks Ambrose <brooksambrose@gmail.com>
2017-12-02 08:29:00 -0800

    Brooks ballot (#10)
    * deploy
    
    * deploy

--- _build.sh
+++ _build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-pkill caddy
-rm -rf _book/ _main*md
-Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
-
+cd /home/rstudio/ && rm -rf _book/ _main*md \
+&& Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
